8a6c51e1fdc43df4ac839b8c86b04618b2422e12
UJJWAL SINGH <ujjwalsingh00013@gmail.com>
2023-01-16 11:55:48 +0530

    added cart modal

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
@@ -5,6 +6,7 @@ import CartProvider from './store/CartProvider';
 function App() {
   return (
     <CartProvider>
+      <Cart/>
       <Header />
       <main>
         <Meals />
